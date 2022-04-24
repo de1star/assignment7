44e6c5d4f4e9b9db04bc5cce8ea1e43760cc0042
Xinchi <843414674@qq.com>
2022-04-24 10:53:27 -0400

    modifications

--- textureMain.js
+++ textureMain.js
@@ -75,6 +75,9 @@ function setUpTextures(){
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    procTexture = gl.createTexture();
+    gl.bindTexture (gl.TEXTURE_2D, procTexture);
 }
 
 //
